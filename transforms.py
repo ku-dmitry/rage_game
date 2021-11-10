deca4e564c70134a02941a3482e8c698f8250053
Dmitry K <dmitry@kuprianov.su>
2021-11-10 18:09:45 +0300

    fancier power realisation

--- transforms.py
+++ transforms.py
@@ -17,7 +17,7 @@ def perspective(self, x, y):
     diff_x = x - self.perspective_point_x
     diff_y = self.perspective_point_y - lin_y
     factor_y = diff_y / self.perspective_point_y
-    factor_y = pow(factor_y, 3)
+    factor_y = factor_y ** 3
     tr_x = self.perspective_point_x + diff_x * factor_y
     tr_y = self.perspective_point_y - factor_y * self.perspective_point_y
     return int(tr_x), int(tr_y)
